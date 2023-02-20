ca8b5f3b1c615552a21e388090dd0d5956b67667
EagerProgrammer <yth960617@unist.ac.kr>
2023-02-20 15:58:46 +0900

    Initial commit

---
+++ src/main/java/com/example/jenkins_test_deploy/JenkinsTestDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_test_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestDeployApplication.class, args);
+    }
+
+}
